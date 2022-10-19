4b3f8225fc612411f2b38edb133628183967ee85
Felixgz <felixgz1c@gmail.com>
2022-10-19 08:30:10 +0200

    19-10-22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContadorModule } from './Contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
  
 import { HeroesModule } from './heroes/heroes.module';
 
@@ -16,7 +17,8 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     AppRoutingModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,30 @@
+import { Component  } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+  
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent{
+
+  personajes: Personaje[] = [  
+    {
+      nombre:'Goku',
+      poder: 9000
+    },
+    {
+      nombre:'Vegeta',
+      poder:8500
+    },
+    {
+      nombre:'Krillin',
+      poder:1000
+    }
+  ]
+
+  nuevo:Personaje = {
+    nombre:'Maestro Roshi',
+    poder: 1000
+  }
+}
